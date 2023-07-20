30a3040aa5df5dc3cf6817606aadb33ebabf077f
abdukarim <abdukarimuz@mail.ru>
2023-07-20 13:55:13 +0500

    Initial commit

---
+++ app/src/main/java/com/vasertoker/firebaseauth/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.vasertoker.firebaseauth
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.vasertoker.firebaseauth.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var googleSignInClient: GoogleSignInClient
+    lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        var signInRequest = BeginSignInRequest.builder()
+//            .setGoogleIdTokenRequestOptions(
+//                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+//                    .setSupported(true)
+//                    // Your server's client ID, not your Android client ID.
+//                    .setServerClientId(getString(R.string.your_web_client_id))
+//                    // Only show accounts previously used to sign in.
+//                    .setFilterByAuthorizedAccounts(true)
+//                    .build()
+//            )
+//            .build()
+
+        val build = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, build)
+        auth = FirebaseAuth.getInstance()
+
+        binding.signInBtn.setOnClickListener {
+            signIn()
+        }
+
+    }
+
+    private var RC_SIGN_IN = 1
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val accountId = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "onActivityResult: firebaseAuthWithGoogle : ${accountId.id}")
+                fireBaseAuthWithGoogle(accountId.idToken!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "onActivityResult: Google sign in failed", e)
+            }
+        }
+
+    }
+
+    fun fireBaseAuthWithGoogle(idToken: String) {
+        val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(firebaseCredential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+//                    updateUI(user)
+                    if (user != null) {
+                        Toast.makeText(this, "${user.displayName}", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+//                    updateUI(null)
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+}
